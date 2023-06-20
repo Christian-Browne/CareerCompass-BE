9b261631d5a235473e770cc6b21232690befa0f4
cbrownea <ChristianBrowne33@gmail.com>
2023-06-20 16:34:28 -0700

    Initial commit

---
+++ src/main/java/app/tracker/jobapplicatontracker/JobApplicatonTrackerApplication.java
@@ -0,0 +1,13 @@
+package app.tracker.jobapplicatontracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobApplicatonTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobApplicatonTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/tracker/jobapplicatontracker/JobApplicatonTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package app.tracker.jobapplicatontracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobApplicatonTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
